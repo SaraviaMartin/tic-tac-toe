d1b09285ead3704b20684ab1927031305508bcd1
Juan Saravia <saraviamartin16@hotmail.com>
2023-06-14 12:33:06 -0300

    game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react"
+
 const TURNS = {
   X: 'x',
   O: 'o',
 }
 
-const board = Array(9).fill(null)
+
+
+const Square = ({children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) =>{
+
+    const newBoard = [...board]        
+    newBoard[index] = turn            //con esto hacemos que el board se actualice
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return  (
     <main className="board">
       <h1>Tic-tac-toe</h1>
@@ -13,15 +43,22 @@ function App() {
         {
           board.map((_, index) => {
             return(
-              <div className="cell" key={index}>
-              <span className="cell__content">
-                {index}
-              </span>
-            </div>
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
             )
             
           })
         }
+      </section>
+      
+      <section className="turn">
+        <Square isSelected = {turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected = {turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   )  
