80e2985026eedf062685880420d48ab31df06847
Juan Saravia <saraviamartin16@hotmail.com>
2023-06-15 17:09:39 -0300

    detectando el ganador del juego

--- src/App.jsx
+++ src/App.jsx
@@ -21,19 +21,55 @@ const Square = ({children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS=[
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) //null = no hay ganador, false = empate
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras para ver si X u O resulto ganador
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+        ) {
+          return boardToCheck[a]
+        }
+    }
+    //si no hay ganador
+    return null
+  }
 
   const updateBoard = (index) =>{
-
+    //no actualizamos esta posicion si ya tiene algo
+    if (board[index] || winner ) return
+    //con esto hacemos que el board se actualice
     const newBoard = [...board]        
-    newBoard[index] = turn            //con esto hacemos que el board se actualice
+    newBoard[index] = turn            
     setBoard(newBoard)
-
+    //cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    //revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
   }
 
   return  (
